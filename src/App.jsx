e673f37c6865349aac4889c9fed8518246086bd0
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2024-08-12 08:58:52 +0000

    WIP : Project setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+const App = () => {
+  return  (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={}></Route>
+      <Route path="/" element={}></Route>
+      <Route path="/" element={}></Route>
+      <Route path="/" element={}></Route>
+      <Route path="/" element={}></Route>
+
+
+    </Routes>
+    
+    </BrowserRouter>
+  )
+}
+
+export default App;
