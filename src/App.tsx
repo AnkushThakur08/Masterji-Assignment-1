c129aa2fce22b0c56578fafe88dc4658503e3856
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2024-08-14 08:07:35 +0000

    Component named change to userCarc

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import RandomUser from "./components/RandomUser";
 import RandomJokes from "./components/RandomJokes";
 import CatListing from "./components/CatListing";
 import PageNotFound from "./components/PageNotFound";
+import UserCard from "./components/UserCard";
 
 
 const App = () => {
   return(
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<RandomUser />} / >
-      <Route path="/random-user" element={<RandomUser />} />
+      <Route path="/" element={<UserCard />} / >
+      <Route path="/random-user" element={<UserCard />} />
       <Route path="/random-jokes" element={<RandomJokes />} />
       <Route path="/cats-listing" element={<CatListing />} />
       <Route path="*" element={<PageNotFound />} />
