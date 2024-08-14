eeff5738060b26d29807cd2f2ed89aa7c2fa32f1
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2024-08-14 11:46:14 +0000

    jokes

--- src/components/Jokes/index.tsx
+++ src/components/Jokes/index.tsx
@@ -1,10 +1,39 @@
-import React from "react"
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card"
 
 
 const Jokes:React.FC = () => {
+    const [jokes, setJokes] = useState<string>()
+    const getData = async () => {
+        const data = await axios.get("https://api.freeapi.app/api/v1/public/randomjokes/joke/random")
+        console.log("data", data);
+        setJokes(data?.data?.message)
+    }
+    useEffect(() => {
+        getData()
+    }, [])
     return (
-        <div>
-            <h1>Random Jokes</h1>
+        <div className="flex items-center justify-center min-h-screen bg-white">
+            {jokes && (
+                <Card className="w-[550px] p-8 bg-black border-white">
+
+                    <CardHeader className="items-center">
+                        {/* <Avatar>
+                            <AvatarImage src={user?.data?.picture?.thumbnail ?? "-"} />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar> */}
+                    </CardHeader>
+
+                    <CardContent className="text-center">
+                        {/* <Content user={user} /> */}
+                    </CardContent>
+
+                    <CardFooter>
+                        {/* <Footer user={user} /> */}
+                    </CardFooter>
+                </Card>
+            )}
         </div>
     )
 }
